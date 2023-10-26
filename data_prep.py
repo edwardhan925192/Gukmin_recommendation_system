a1e54510162d2d82de78963e7c14e9535b2a227e
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-10-26 10:41:41 +0900

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -2,6 +2,24 @@ import pandas as pd
 from sklearn.preprocessing import MultiLabelBinarizer
 import numpy as np
 
+def pick_n_ones(row, n):
+    # Get indices of ones
+    ones_indices = np.where(row == 1)[0]
+
+    # If there are fewer than n ones, return the row as is
+    if len(ones_indices) <= n:
+        return row
+
+    # Randomly select n indices to keep as ones
+    keep_indices = np.random.choice(ones_indices, size=n, replace=False)
+
+    # Set all other ones to zero
+    for idx in ones_indices:
+        if idx not in keep_indices:
+            row[idx] = 0
+
+    return row
+
 def one_hot_encode_column(df, column_name):
     # Make a copy of the DataFrame to avoid modifying the original
     df_copy = df.copy()
@@ -81,20 +99,130 @@ def one_hot_encode_date(df, column_name):
 
     return df_copy
 
-def pick_n_ones(row, n):
-    # Get indices of ones
-    ones_indices = np.where(row == 1)[0]
+def extract_unique_words(data, column_name):
+    unique_words = set()
 
-    # If there are fewer than n ones, return the row as is
-    if len(ones_indices) <= n:
-        return row
+    data[column_name].fillna('none', inplace=True)
 
-    # Randomly select n indices to keep as ones
-    keep_indices = np.random.choice(ones_indices, size=n, replace=False)
+    for row in data[column_name]:
+        words = row.split(';')
+        unique_words.update(words)
 
-    # Set all other ones to zero
-    for idx in ones_indices:
-        if idx not in keep_indices:
+    return unique_words
+
+def map_dict(input_list1, input_list2):
+    # Ensure the two lists have the same length
+    if len(input_list1) != len(input_list2):
+        raise ValueError("The two lists must have the same length.")
+
+    # Create the dictionary using a dictionary comprehension
+    result_dict = {input_list1[i]: input_list2[i] for i in range(len(input_list1))}
+
+    return result_dict
+
+def translate(df,column, translation_dict):
+    replaced_values = []
+    for entry in df[column]:
+        # Split the entry by " ; "
+        words = entry.split(";")
+        # Replace each word with its translation from the dictionary
+        translated_words = [translation_dict.get(word, word) for word in words]
+        # Join the translated words back with " ; "
+        replaced_values.append(";".join(translated_words))
+
+    df[column] = replaced_values
+    return df
+
+def map_cluster(df, column, mapping_dict):
+    mapped_values = []
+    for entry in df[column]:
+        # Split the entry by " ; "
+        words = entry.split(";")
+        # Map each word to its corresponding number from the dictionary
+        numbers = [str(mapping_dict.get(word, word)) for word in words]
+        # Join the numbers back with " ; "
+        mapped_values.append(";".join(numbers))
+
+    df[column] = mapped_values
+    return df
+    
+def onehot_full(df, column_name):
+    # One-hot encode the specified column
+    one_hot = pd.get_dummies(df[column_name], prefix=column_name)
+
+    # Drop the original column from the DataFrame
+    df = df.drop(column_name, axis=1)
+
+    # Concatenate the one-hot encoded columns to the original DataFrame
+    df = pd.concat([df, one_hot], axis=1)
+
+    return df
+
+import numpy as np
+import torch.nn.functional as F
+
+# ==================== Input should be in numpy format ===================== #
+def train_prep(matrix, n):
+    matrix_copy = matrix.copy()
+    for row in matrix_copy:
+        ones_indices = np.where(row == 1)[0]
+
+        if len(ones_indices) <= n:
+            continue
+
+        zero_indices = np.random.choice(ones_indices, size=n, replace=False)
+
+        for idx in zero_indices:
             row[idx] = 0
 
-    return row
+    return matrix_copy
+
+def find_ones(matrix):
+    result = []
+    for row in matrix:
+        indices = np.where(row == 1)[0]
+        if indices.size:
+            result.append(tuple(indices))
+        else:
+            result.append('None')
+    return result
+
+def get_matrix(matrix, indexes_list):
+
+    if isinstance(matrix, np.ndarray):
+        matrix = torch.tensor(matrix)
+    # Ensure that the number of rows in the matrix matches the length of indexes_list
+    assert matrix.shape[0] == len(indexes_list), "Number of rows in matrix must match length of indexes_list"
+
+    # Process each row in the matrix
+    for i in range(matrix.shape[0]):
+        # Calculate the number of indexes for the current row
+        num_indexes = len(indexes_list[i])
+
+        # Get the indices of the top values for the current row
+        _, top_indices = torch.topk(matrix[i], num_indexes)
+
+        # Create a mask with 0s everywhere
+        mask = torch.zeros_like(matrix[i])
+
+        # Set the top indices in the mask to 1
+        mask[top_indices] = 1
+
+        # Update the row using the mask
+        matrix[i] = mask
+
+    return matrix
+
+def bce_loss(output, target):
+
+    # Compute the binary cross-entropy loss without reduction
+    loss = F.binary_cross_entropy_with_logits(output, target, reduction='none')
+
+    # Create a mask where the target matrix has 1s
+    mask = target == 1
+
+    # Apply the mask to the loss
+    masked_loss = loss * mask.float()
+
+    # Compute and return the final loss value
+    return masked_loss.sum() / mask.sum()
